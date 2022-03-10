389f7a000edeab081c4566ff43033c75874c69f4
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 17:16:35 -0500

    Add notImplemented error

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -18,15 +18,7 @@ class Parser:
         return file_ext in self.file_exts
 
     def parse(self, path: Path, source: Path, dest: Path):
-        content = Content.load(self.read(path))
-        html = markdown(content.body)
-        self.write(path, dest, html)
-        filtered = hooks.filter("generate_menu", html, self.base_ext)
-        self.write(path, dest, filtered)
-        sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
-        )
-        hooks.event("written")
+        raise NotImplementedError
 
     def read(self, path):
         with open(path, "r") as file:
