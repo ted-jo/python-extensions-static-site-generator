73001f5c8bbe464399f5945678bca532344fa26d
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 17:21:16 -0500

    time typo

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -7,7 +7,7 @@ total_written = 0
 @hooks.register("start_build")
 def start_build():
     global start_time 
-    start_time = time.tiem()
+    start_time = time.time()
 
 @hooks.register("written")
 def written():
