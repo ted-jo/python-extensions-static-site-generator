4a53d2558a5f1890f2cc35c3a33731bf408df56b
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 14:20:45 -0500

    load_directory function created

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -6,3 +6,7 @@ def load_module(directory, name):
     sys.path.insert(0, directory)
     import_module(name)
     sys.path.pop(0)
+
+def load_directory(directory):
+    for path in directory.Path.rglob("*.py"):
+        load_module(directory.as_posix(), path.stem)
