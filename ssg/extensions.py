c7b5b53c6c10f1fb24988817d486596bfb697feb
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 14:22:35 -0500

    load_bundled function created

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -10,3 +10,7 @@ def load_module(directory, name):
 def load_directory(directory):
     for path in directory.Path.rglob("*.py"):
         load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
