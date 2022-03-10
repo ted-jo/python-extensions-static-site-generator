8ba0e2af16c6904c1173b5d1b70feeea7124b7ad
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 16:44:55 -0500

    fixed isinstance

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,7 +4,7 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p : not p.isinstance(parsers.ResourceParser)
+    valid = lambda p : not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
