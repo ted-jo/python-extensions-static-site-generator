070584c6ec961642b6585d15da9a0c5fb985406e
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 17:20:03 -0500

    Fixed stupid +1 schema

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -7,12 +7,12 @@ total_written = 0
 @hooks.register("start_build")
 def start_build():
     global start_time 
-    start_time = time.localtime()
+    start_time = time.tiem()
 
 @hooks.register("written")
 def written():
     global total_written
-    total_written += 1
+    total_written = total_written  + 1
 
 @hooks.register("stats")
 def stats():
